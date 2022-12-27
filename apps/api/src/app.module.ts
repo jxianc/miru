0b4ec0e0604172222fd74794c8fb415228293aeb
Jingxian <jingxianchai01@gmail.com>
2022-12-27 01:32:52 -0600

    added graphql

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { AppResolver } from './app.resolver'
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      context: ({ req, res }) => ({ req, res }), // attach request and response to graphql context
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(process.env.PORT)
+  console.log(
+    `[Miru]: Server is currently running at http://localhost:${process.env.PORT}/graphql`,
+  )
 }
 bootstrap()
